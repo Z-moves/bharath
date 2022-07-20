160e5e15bf7036b39a14e2c0257c793ac3e69552
rootedcyber93 <108932489+rootedcyber93@users.noreply.github.com>
2022-07-20 22:53:27 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -11,6 +11,7 @@ REDIS_PASSWORD=002n7vKob6MBs5xGO445wUgem94vG6xU
 SESSION=1BVtsOG4Bu8OxyjW9iVqUP5xGkJLQ-AAWolpNK1gZ1P53AZO0-uFyjG-Z50ng5KruwO9Ka9QxVTrXZd-JZkiqbzP_W-iFDv-bne4Kr4CPd-g1INbKGs0ByNcLAQjabhxd4_kgBhHUArn_OG9sNq0yeuO5xHcwRI7SxoAJhaSB6cpBlzF2IkOMjykS-CLCYV9wpLVJA4dKptOTQOxx_UKLPxedExoTvbD5dvxVXEKl4gjb8ICmsonBc2ZpfM48aiLUMEiwnKPlahiYys8Q9Mgp1XYtaTIiNQfl57avRc7iLA8_dpVqiFuzXoTt3iNF3BgMICwrdwcakSfzXFChZgMyJkawjjTGlH0=
 EOF
 chmod -R 777 .*
-pip3 install -r req*
-pip3 install -r res*/st*/op*
+#pip3 install -r req*
+#pip3 install -r res*/st*/op*
+docker
 bash startup

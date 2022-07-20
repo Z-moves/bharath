f59ecd5551a12ce14cb36949f97787e3336983fb
rootedcyber93 <108932489+rootedcyber93@users.noreply.github.com>
2022-07-20 18:45:31 +0530

    Update and rename bharat.sh to action.sh

--- action.sh
+++ action.sh
@@ -1,3 +1,5 @@
+pwd
+ls
 git clone https://github.com/TeamUltroid/Ultroid
 cd Ultroid
 cat >> .env << EOF
@@ -8,4 +10,6 @@ REDIS_PASSWORD=002n7vKob6MBs5xGO445wUgem94vG6xU
 SESSION=1BVtsOG4Bu8OxyjW9iVqUP5xGkJLQ-AAWolpNK1gZ1P53AZO0-uFyjG-Z50ng5KruwO9Ka9QxVTrXZd-JZkiqbzP_W-iFDv-bne4Kr4CPd-g1INbKGs0ByNcLAQjabhxd4_kgBhHUArn_OG9sNq0yeuO5xHcwRI7SxoAJhaSB6cpBlzF2IkOMjykS-CLCYV9wpLVJA4dKptOTQOxx_UKLPxedExoTvbD5dvxVXEKl4gjb8ICmsonBc2ZpfM48aiLUMEiwnKPlahiYys8Q9Mgp1XYtaTIiNQfl57avRc7iLA8_dpVqiFuzXoTt3iNF3BgMICwrdwcakSfzXFChZgMyJkawjjTGlH0=
 EOF
 chmod -R 777 .*
+pip3 install -r req*
+pip3 install -r res*/st*/op*
 bash startup
